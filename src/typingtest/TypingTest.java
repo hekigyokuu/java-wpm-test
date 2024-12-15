012f0c932739ab18b793791cd2f9867483911eae
hekigyokuu <hekiigyokuu@gmail.com>
2024-12-15 09:55:37 +0800

    added running method

--- src/typingtest/TypingTest.java
+++ src/typingtest/TypingTest.java
@@ -15,6 +15,34 @@ public class TypingTest {
         return TEXT_TO_INPUT[rand.nextInt(TEXT_TO_INPUT.length)];
     }
 
+    private void displayTestToInput(String textToInput) {
+        System.out.println("Type the text as fast as you can!");
+        System.out.println("\n" + textToInput + "\n");
+    }
+
+    private void confirmToStart(Scanner sc) {
+        System.out.println("Press Enter to start...");
+        sc.nextLine();
+    }
+
+    private String scanUserInput(Scanner sc) {
+        System.out.println("Start typing now...");
+        return sc.nextLine();
+    }
+
+    public void startTypingTest() {
+        Scanner sc = new Scanner(System.in);
+
+        String textToInput = randomTextGenerator();
+        displayTestToInput(textToInput);
+
+        confirmToStart(sc);
+
+        long timeStart = System.currentTimeMillis();
+        String userInput = scanUserInput(sc);
+        long timeEnd = System.currentTimeMillis();
+    }
+
     public static void main(String[] args) {
         TypingTest type = new TypingTest();
         System.out.println(type.randomTextGenerator());
