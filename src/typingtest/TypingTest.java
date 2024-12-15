8406a35cd0a813c2f81b65381f0901b1e49ca7f5
hekigyokuu <hekiigyokuu@gmail.com>
2024-12-15 10:32:34 +0800

    added calculation of accuracy, wpm, and time taken

--- src/typingtest/TypingTest.java
+++ src/typingtest/TypingTest.java
@@ -5,9 +5,11 @@ import java.util.Random;
 
 public class TypingTest {
     private static final String[] TEXT_TO_INPUT = {
-        "Life is really simple, but we insist on making it complicated.",
-        "The only limit to our realization of tomorrow will be our doubts of today.",
-        "Courage is grace under pressure."
+        "life is really simple, but we insist on making it complicated",
+        "the only limit to our realization of tomorrow will be our doubts of today",
+        "courage is grace under pressure",
+        "it does not matter how slowly you go as long as you do not stop",
+        "in the middle of difficulty lies opportunity"
     };
 
     private String randomTextGenerator() {
@@ -30,6 +32,34 @@ public class TypingTest {
         return sc.nextLine();
     }
 
+    private double calcElapsedTime(long timeStart, long timeEnd) {
+        return (timeEnd - timeStart);
+    }
+
+    private int calcWPM(String userInput, double elapsedTime) {
+        int totalCharacters = userInput.length();
+        double seconds = elapsedTime / 1000;
+        int wpm = (int) ((totalCharacters / 5) / seconds * 60);
+        return wpm;
+    }
+
+    private double calcAccuracy(String textToInput, String userInput) {
+        int correctChar = 0;
+        for (int i = 0; i < Math.min(textToInput.length(), userInput.length()); i++) {
+            if (textToInput.charAt(i) == userInput.charAt(i)) {
+                correctChar++;
+            }
+        }
+        return (correctChar / (double) textToInput.length()) * 100;
+    }
+
+    private void results(double elapsedTime, int wpm, double accuracy) {
+        System.out.println("Results: ");
+        System.out.println("Time Taken: " + elapsedTime + "milliseconds (" + (elapsedTime/1000) + " seconds)");
+        System.out.println("Word/s per Minute: " + wpm);
+        System.out.println("Accuracy: " + accuracy + "%");
+    }
+
     public void startTypingTest() {
         Scanner sc = new Scanner(System.in);
 
@@ -41,10 +71,16 @@ public class TypingTest {
         long timeStart = System.currentTimeMillis();
         String userInput = scanUserInput(sc);
         long timeEnd = System.currentTimeMillis();
+
+        double elapsedTime = calcElapsedTime(timeStart, timeEnd);
+        int wpm = calcWPM(userInput, elapsedTime);
+        double accuracy = calcAccuracy(textToInput, userInput);
+
+        results(elapsedTime, wpm, accuracy);
     }
 
     public static void main(String[] args) {
         TypingTest type = new TypingTest();
-        System.out.println(type.randomTextGenerator());
+        type.startTypingTest();
     }
 }
